059f004f9d6ff4d9806c6c6b44eed3edaa04f54f
Pranav-Shivam <21MCA2618@cuchd.in>
2022-06-22 00:35:09 +0530

    First Commit

---
+++ blood donation/adddonor.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+public partial class adddonor : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security = True");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand cmd = con.CreateCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "insert into blood values('"+TextBox1.Text+"','"+TextBox2.Text+"','"+TextBox3.Text+"','"+TextBox4.Text+"')";
+        cmd.ExecuteNonQuery();
+        Response.Write("<script>alert('record inserted successfully');</script>");
+        con.Close();
+    }
+}
---
+++ blood donation/viewdonor.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class viewdonor : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        show_data();
+    }
+
+    private void show_data()
+    {
+        DataTable dt = new DataTable();
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand("select * from blood", con);
+        con.Open();
+        SqlDataReader dr = cmd.ExecuteReader();
+        dt.Load(dr);
+        GridView1.DataSource = dt;
+        GridView1.DataBind();
+        con.Close();
+    }
+}
